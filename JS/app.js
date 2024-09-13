0b04f778cfb3f06d96a221724919514d2ff0d190
the-mostest <fbmcdougall@gmail.com>
2024-09-13 17:41:58 +0100

    grid, homepage and settings framwork done (need to look at buttons

--- JS/app.js
+++ JS/app.js
@@ -1,32 +1,41 @@
 
 
 
-const settingPopUp = () => {
-    settingsPage.classList.toggle('popup')
-    if (settingsPage.classList == 'popup') {
-        settingsPage.classList.remove('popup')
-    }
-}
-
-
-
-
-
-
 // Elemet Cache
 const settingsPage = document.querySelector('#settingsPage')
-const settingsBut = document.querySelector('#settingsButton')
+const settingsButEl = document.querySelector('.settingsBut') 
 const exitSet = document.querySelector('.exitSettings')
+const gameBut = document.querySelector('#gameButton')
+const gamePage = document.querySelector('#gamepage')
+const homeButtonEl = document.querySelector('.homeButton')
+const grid = document.querySelector('.grid')
 
-
+console.dir(settingsButEl)
 
 // Const
 
 
 
+const cellStore = []
+
+
 // Grid Creation
 
+const width = 57;
+const height = 41;
+let totalCells = width * height
 
+
+for (i = 0; i < totalCells; i++) {
+    const cellEl = document.createElement('div')
+    cellEl.id = i
+    // cellEl.innerText = i
+    cellEl.classList.add('cell')
+    cellEl.style.height = `${100 / height}%`
+    cellEl.style.width = `${100 / width}%`
+    cellStore.push(cellEl)
+    grid.appendChild(cellEl);
+}
 
 
 
@@ -36,7 +45,31 @@ const exitSet = document.querySelector('.exitSettings')
 
 
 
+
+
+//  Functions
+const settingPopUp = () => {
+    settingsPage.classList.toggle('popup')
+    if (settingsPage.classList == 'popup') {
+        settingsPage.classList.remove('popup')
+    }
+}
+
+const startGame = () => {
+    gamePage.classList.toggle('popup')
+}
+
+const homeBut = () => {
+    settingsPage.classList.toggle('popup')
+    gamePage.classList.remove('popup');
+
+}
+
+
+
 // Event Listener
 
-settingsBut.addEventListener('click', settingPopUp);
+settingsButEl.addEventListener('click', settingPopUp);
 exitSet.addEventListener('click', settingPopUp);
+gameBut.addEventListener('click', startGame);
+homeButtonEl.addEventListener('click', homeBut);
