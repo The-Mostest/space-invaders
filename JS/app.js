acd2dbe1a8f8061562e21e85ccaa5f621af593d5
the-mostest <fbmcdougall@gmail.com>
2024-09-17 14:00:58 +0100

    Stuck on how to get player to not travel through walls

--- JS/app.js
+++ JS/app.js
@@ -22,7 +22,7 @@ const grid = document.querySelector('.grid')
 const cellStore = []
 let enemyPosition = [25, 26, 27, 28, 29, 30, 31, 32, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 250, 251, 252,  255, 256, 257, 258,  261, 262, 263, 306, 307, 308, 309, 312, 313, 314, 315,  318, 319, 320, 321, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 420, 421, 422, 423, 424, 425, 426,429, 430, 431, 432, 433, 434, 435, 477, 478, 479, 480, 481, 482, 483, 486, 487, 488, 489, 490, 491, 492, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547, 548, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 651,652,655,656,659,660,708,709,712,713,716,717,766,765,769,770,773,774]
 
-
+let playerPosition = [2191, 2193, 2195, 2249, 2250, 2251, 2305, 2309]
 
 
 
@@ -44,6 +44,9 @@ for (i = 0; i < totalCells; i++) {
 
    if(enemyPosition.indexOf(i) !== -1) {
     cellEl.classList.add('enemy')
+   }
+   if(playerPosition.indexOf(i) !== -1) {
+    cellEl.classList.add('player')
    }
 
     cellStore.push(cellEl)
@@ -89,37 +92,72 @@ const homeBut = () => {
 
 
 
-
-const removeEnemyPos = () => {
-    enemyPosition.forEach((position) => {
-        cellStore[position].classList.remove('enemy')
-    })
+const removePlayerPosition = () => {
+    playerPosition.forEach((position) => {
+        cellStore[position].classList.remove('player')
+})
 }
 
 
-const addEnemyPos = () => {
-    enemyPosition.forEach((position) => {
-        cellStore[position].classList.add('enemy')
-    })
+const addPlayerPosition = () => {
+    playerPosition.forEach((position) => {
+        cellStore[position].classList.add('player')
+})
+}
+
+const playerMovement = (evt) => {
+
+    removePlayerPosition(); 
+    
+    const hasHitWall = playerPosition.some(position => evt.code === 'ArrowRight' && position % width === width -1)
+
+
+    if (evt.code === 'ArrowRight' && playerPosition[i] % width !== width -1) {
+                playerPosition[i]++
+            } else { hasHitWall
+
+    }if (evt.code === 'ArrowLeft' && playerPosition[i] % width !== 0){
+                playerPosition[i]--
+            }
+
+    
+    addPlayerPosition();
+
 }
 
 
-    // setInterval(enemyMovement, 1000)
+console.log(playerPosition)
 
 
-const enemyTimer = () => {
-   removeEnemyPos()
-   enemyPosition.forEach((position) => {
-       if(position % width !== width -1){
-        position++
-       }
 
-   })
-   addEnemyPos;
-}
-setInterval(enemyTimer,1000);
 
-console.dir(enemyPosition)
+
+
+
+
+
+
+// const removeEnemyPos = () => {
+//     enemyPosition.forEach((position) => {
+//         cellStore[position].classList.remove('enemy')
+//     })
+// }
+
+
+// const addEnemyPos = () => {
+//     enemyPosition.forEach((position) => {
+//         cellStore[position].classList.add('enemy')
+//     })
+// }
+
+
+// const enemyMovement = () => {
+//     if(cellStore ==='') {
+//         enemyPosition.id ++
+//     }
+// }
+
+
 
 
 
@@ -143,3 +181,37 @@ settingsButEl.forEach((setbut1) => {
 exitSet.addEventListener('click', settingPopUp);
 gameBut.addEventListener('click', startGame,);
 homeButtonEl.addEventListener('click', homeBut);
+
+document.addEventListener('keydown', playerMovement)
+
+
+
+
+
+
+
+// const removePlayerPosition = () => {
+//     playerPosition.forEach((position) => {
+//         cellStore[position].classList.remove('player')
+// })
+// }
+
+
+// const addPlayerPosition = () => {
+//     playerPosition.forEach((position) => {
+//         cellStore[position].classList.add('player')
+// })
+// }
+
+// const playerMovement = (evt) => {
+
+//     removePlayerPosition(); 
+
+//   for( let i = 0; i < playerPosition.length; i++)
+//     { if (evt.code === 'ArrowRight' && playerPosition[i] % width !== width -1) {
+//         playerPosition[i]++
+//     } else if (evt.code === 'ArrowLeft' && playerPosition[i] % width !== 0){
+//         playerPosition[i]--
+//     }}
+    
+//     addPlayerPosition();
