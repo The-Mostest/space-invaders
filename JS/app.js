355af91155c4ce0b39693c203ffff2a8d928a6ad
the-mostest <fbmcdougall@gmail.com>
2024-09-20 15:48:15 +0100

    20.09.23

--- JS/app.js
+++ JS/app.js
@@ -21,16 +21,16 @@ const cellStore = [];
 
 let enemyPosition = [
   25, 26, 27, 28, 29, 30, 31, 32, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-  91, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 193, 194,
-  195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 250, 251, 252,
-  255, 256, 257, 258, 261, 262, 263, 306, 307, 308, 309, 312, 313, 314, 315,
-  318, 319, 320, 321, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373,
-  374, 375, 376, 377, 378, 420, 421, 422, 423, 424, 425, 426, 429, 430, 431,
-  432, 433, 434, 435, 477, 478, 479, 480, 481, 482, 483, 486, 487, 488, 489,
-  490, 491, 492, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546,
-  547, 548, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 651,
-  652, 655, 656, 659, 660, 708, 709, 712, 713, 716, 717, 766, 765, 769, 770,
-  773, 774,
+  91, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 193, 194, 195,
+  196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 250, 251, 252, 255,
+  256, 257, 258, 261, 262, 263, 306, 307, 308, 309, 312, 313, 314, 315, 318,
+  319, 320, 321, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374,
+  375, 376, 377, 378, 420, 421, 422, 423, 424, 425, 426, 429, 430, 431, 432,
+  433, 434, 435, 477, 478, 479, 480, 481, 482, 483, 486, 487, 488, 489, 490,
+  491, 492, 535, 536, 537, 538, 539, 540, 541, 542, 543, 544, 545, 546, 547,
+  548, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 651, 652,
+  655, 656, 659, 660, 708, 709, 712, 713, 716, 717, 766, 765, 769, 770, 773,
+  774,
 ];
 
 let playerPosition = [2191, 2193, 2195, 2249, 2250, 2251, 2305, 2309];
@@ -38,7 +38,7 @@ let playerPosition = [2191, 2193, 2195, 2249, 2250, 2251, 2305, 2309];
 let enemyMissile = [];
 
 let shootSound = new Audio("CSS/Media/Audio/SFX/Shooting.mov");
-let explosionSound = new Audio("CSS/Media/Audio/SFX/EnemyExplode.mov")
+let explosionSound = new Audio("CSS/Media/Audio/SFX/EnemyExplode.mov");
 
 let score = 0;
 
@@ -86,15 +86,15 @@ const homeBut = () => {
 };
 
 const gameWin = () => {
-  winScreen.classList.add('popup')
-}
+  winScreen.classList.add("popup");
+};
 
 const scoreUpdate = () => {
-  score += 100
-  scoreBoard.forEach((board)=> {
-    board.innerHTML = score
-  })
-}
+  score += 100;
+  scoreBoard.forEach((board) => {
+    board.innerHTML = score;
+  });
+};
 
 // -------------------------- Functions END ------------------------------
 
@@ -135,7 +135,6 @@ const playerMovement = (evt) => {
 };
 // -------------------------- PLayer MOving end ------------------------------
 
-
 // -------------------------- Missiles Shooting ------------------------------
 
 const playerShooting = (evt) => {
@@ -165,21 +164,14 @@ const playerShooting = (evt) => {
         return;
       }
       cellStore[missilePosition].classList.add("playerMissile");
-      if (enemyPosition.length === 0){
-        gameWin()
+      if (enemyPosition.length === 0) {
+        gameWin();
       }
     }, 100);
   }
-
 };
 
-
 // -------------------------------------------------------------------
-
-
-
-
-
 
 // -------------------------- Missiles Shooting end---------------------------
 
@@ -233,11 +225,6 @@ const enemyMovement = () => {
   });
 };
 
-
-
-
-
-
 const gameOver = () => {
   gameOverScreen.classList.add("popup");
 
@@ -255,8 +242,6 @@ cellStore.forEach((cell) => {
 });
 
 // ------------------ ENEMY MOVEMENT END ---------------------------------
-
-
 
 // --------------------Reset Button---------------------------------------
 const resetting = () => {
@@ -284,8 +269,8 @@ const resetting = () => {
   settingsPage.classList.remove("popup");
   gamePage.classList.remove("popup");
   gameOverScreen.classList.remove("popup");
-  winScreen.classList.remove("popup")
-  score = 0
+  winScreen.classList.remove("popup");
+  score = 0;
   clearInterval(enemyInterval);
 };
 
@@ -313,8 +298,8 @@ document.addEventListener("keydown", playerMovement);
 document.addEventListener("keydown", playerShooting);
 
 backgroundMusic.volume = 0.01;
-explosionSound.volume= 0.01;
-shootSound.volume= 0.01;
+explosionSound.volume = 0.01;
+shootSound.volume = 0.01;
 volumeSlider.addEventListener("click", () => {
   backgroundMusic.volume = volumeSlider.value;
   explosionSound.volume = volumeSlider.value;
